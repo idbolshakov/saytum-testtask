8658b55843dce92b13e78e4a3ab3b0995c2e9499
Ivan Bolshakov <idbolshakov@gmail.com>
2016-10-10 19:43:14 +0500

    Formed MailSender config format

--- index.php
+++ index.php
@@ -7,7 +7,13 @@ require('userData.php');
 $mailSenderConfig = array(
 
     'sorting_key'        => 'date_registration',
-    'sorting_ascending'  => 'true'
+    'sorting_ascending'  => 'true',
+    'domain_restriction' => 'true',
+    'allow_domains'      => array('gmail.com', 'saytum.ru'),
+    'time_restriction'   => 'true',
+    'allow_time'         => array('min_time' => 'time', 'max_time' => 'time'),
+    'age_restriction'    => 'true',
+    'allow_age'          => array('min_age' => 'age', 'max_age' => 'age'),
 );
 
 Saytum\MailSender\MailSender::getInstance()
